5e4f3a6da8858ee0160b2e5f7bd5e972120f1faa
Whitehercog <61663639+Joelwallker@users.noreply.github.com>
2023-12-25 12:55:41 +0300

    add helm

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -10,6 +10,18 @@ provider "kubernetes" {
   }
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-id", module.eks.cluster_id]
+      command     = "aws"
+    }
+  }
+}
+
 locals {
   cluster_name = var.cluster_name
 }
